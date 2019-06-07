29c78ce25f3046abfe4c4385c436c62c3dc560c2
jerome1232 <j.jones1232@gmail.com>
2019-06-07 04:03:18 -0700

    fixed php img tag

--- webpage/startpi.php
+++ webpage/startpi.php
@@ -37,7 +37,8 @@
     echo "If you kill the python script, just reload the page it will be run automatically again";
     echo "</p>"
     $ip = $_SERVER['SERVER_ADDR'];
-    echo '<img src="http://$ip:8080/?action=stream" />'
+    $img = "http://" . $ip . ":8080/?action=stream"
+    echo "<img src='$img' />"
 ?>
 <div id="overlay">
 </div>

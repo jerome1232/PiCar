c2f82ae87043598b61df19bfd922c993f332c393
jerome1232 <j.jones1232@gmail.com>
2019-06-12 11:24:42 -0700

    Converted all soft tabs to hard tabs

--- webpage/keys.js
+++ webpage/keys.js
@@ -3,181 +3,181 @@
 // what I do know, is that document.addEventListener causes
 // it to start listening for key strokes ;)
 document.addEventListener('keydown', function(keyDown) {
-  // This is grabbing a canvas object from the webpage
-  // (see index.php to see where the canvas object comes from)
-  // at anyrate this grabs that canvas object and sets it up
-  // as a 2d drawing area.
-  var canvas = document.getElementById("keyPressed");
-  var ctx = canvas.getContext("2d");
-  ctx.font = "30px Arial";
-  // each of these if branches are pretty much the same
-  // when a key is pressed, I clear what's in my canvas object
-  // and write something to it. This is just for testing atm.
-  // I intend in the future to call a php script that writes
-  // what key was pressed to a unix domain socket.
-  var x = canvas.width / 2;
-  if (keyDown.keyCode == 65) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "aDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('A: Turning left', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 68) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "dDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('D: Turning right', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 87) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "wDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('W: Moving forward', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 83) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "sDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('S: Moving backwords', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 32) {
-  $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "spaceDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('Space: Stopping', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 81) {
-  $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "qDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('Q: Killing python script', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 82) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "rDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('R: Increasing speed', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 70) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "fDown"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('F: Decreasing speed', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyDown.keyCode == 72) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "hDown"},
-      success: fucntion(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('Honking the horn', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    })
-  }
+	// This is grabbing a canvas object from the webpage
+	// (see index.php to see where the canvas object comes from)
+	// at anyrate this grabs that canvas object and sets it up
+	// as a 2d drawing area.
+	var canvas = document.getElementById("keyPressed");
+	var ctx = canvas.getContext("2d");
+	ctx.font = "30px Arial";
+	// each of these if branches are pretty much the same
+	// when a key is pressed, I clear what's in my canvas object
+	// and write something to it. This is just for testing atm.
+	// I intend in the future to call a php script that writes
+	// what key was pressed to a unix domain socket.
+	var x = canvas.width / 2;
+	if (keyDown.keyCode == 65) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "aDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('A: Turning left', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 68) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "dDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('D: Turning right', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 87) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "wDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('W: Moving forward', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 83) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "sDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('S: Moving backwords', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 32) {
+	$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "spaceDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('Space: Stopping', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 81) {
+	$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "qDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('Q: Killing python script', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 82) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "rDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('R: Increasing speed', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 70) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "fDown"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('F: Decreasing speed', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyDown.keyCode == 72) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "hDown"},
+			success: fucntion(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('Honking the horn', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		})
+	}
 }, true);
 
 document.addEventListener("keyup", function(keyUp) {
-  var canvas = document.getElementById("keyPressed");
-  var ctx = canvas.getContext("2d");
-  ctx.font = "30px Arial";
-  var x = canvas.width / 2;
+	var canvas = document.getElementById("keyPressed");
+	var ctx = canvas.getContext("2d");
+	ctx.font = "30px Arial";
+	var x = canvas.width / 2;
 
-  if (keyUp.keyCode == 65) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "aUp"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('A: Cease the turn!', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyUp.keyCode == 68) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "dUp"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('D: Cease the turn!', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyUp.keyCode == 87) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "wUp"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('W: Cease the forward!', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    });
-  }
-  else if (keyUp.keyCode == 83) {
-    $.ajax({
-      type: 'POST',
-      url: 'sock_write.php',
-      data: {key: "sUp"},
-      success: function(data) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText('S: Cease the backward!', 10, 50);
-        ctx.fillText(data, 10, 80);
-      }
-    })
-  }
+	if (keyUp.keyCode == 65) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "aUp"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('A: Cease the turn!', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyUp.keyCode == 68) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "dUp"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('D: Cease the turn!', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyUp.keyCode == 87) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "wUp"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('W: Cease the forward!', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		});
+	}
+	else if (keyUp.keyCode == 83) {
+		$.ajax({
+			type: 'POST',
+			url: 'sock_write.php',
+			data: {key: "sUp"},
+			success: function(data) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.fillText('S: Cease the backward!', 10, 50);
+				ctx.fillText(data, 10, 80);
+			}
+		})
+	}
 }, true);
--- webpage/startpi.php
+++ webpage/startpi.php
@@ -1,60 +1,60 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <link rel="stylesheet" href="styles.css" />
-    <meta name="viewport"
-        content="viewport"
-        charset="UTF-8">
+		<link rel="stylesheet" href="styles.css" />
+		<meta name="viewport"
+				content="viewport"
+				charset="UTF-8">
 </head>
 <body>
-  <div class="topnav">
-      <a href="index.html">Home</a>
-      <a class="active" href="startpi.php">PiCar</a>
-      <a href="joystick.html">Joystick Testing</a>
-      <a href="downloads.php">Downloads</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-  </div>
+	<div class="topnav">
+			<a href="index.html">Home</a>
+			<a class="active" href="startpi.php">PiCar</a>
+			<a href="joystick.html">Joystick Testing</a>
+			<a href="downloads.php">Downloads</a>
+			<a href="#contact">Contact</a>
+			<a href="#about">About</a>
+	</div>
 
 
-  <?php
-    $ip = $_SERVER['SERVER_ADDR'];
-    echo "<br><br>";
-    echo "<img src=\"http://$ip:8080/?action=stream\" />";
-    echo "<p>";
-    echo shell_exec('uptime -p');
-    echo "<br>";
-    echo php_uname();
-    echo "<br>";
-    $pidf_path = "tmp/pid";
-    $pidf = fopen($pidf_path, "r");
-    $pid = fread($pidf, filesize($pidf_path));
-    fclose($pidf);
-    echo "Previous PID: $pid, Killing it to ensure it is dead<br>";
-    shell_exec("kill $pid");
-    $cmd = "nohup ./robot_car.py >/dev/null 2>&1 &";
-    echo "Starting python script<br>";
-    shell_exec($cmd);
-    echo "Script Started, ready to roll<br>";
-    echo "If you kill the python script, just reload the page "
-      . "it will be run automatically again";
-    echo "<br>";
-    echo "server ip: $ip";
-    echo "</p>";
-  ?>
+	<?php
+		$ip = $_SERVER['SERVER_ADDR'];
+		echo "<br><br>";
+		echo "<img src=\"http://$ip:8080/?action=stream\" />";
+		echo "<p>";
+		echo shell_exec('uptime -p');
+		echo "<br>";
+		echo php_uname();
+		echo "<br>";
+		$pidf_path = "tmp/pid";
+		$pidf = fopen($pidf_path, "r");
+		$pid = fread($pidf, filesize($pidf_path));
+		fclose($pidf);
+		echo "Previous PID: $pid, Killing it to ensure it is dead<br>";
+		shell_exec("kill $pid");
+		$cmd = "nohup ./robot_car.py >/dev/null 2>&1 &";
+		echo "Starting python script<br>";
+		shell_exec($cmd);
+		echo "Script Started, ready to roll<br>";
+		echo "If you kill the python script, just reload the page "
+			. "it will be run automatically again";
+		echo "<br>";
+		echo "server ip: $ip";
+		echo "</p>";
+	?>
 <script src='jquery/jquery-3.3.1.js'></script>
 <script src='keys.js'></script></body>
 <p>
 <canvas id="keyPressed")
-  width="600px"
-  height="100px">
+	width="600px"
+	height="100px">
 </canvas>
 </p>
 </body>
 <footer>
-  <address>
-    Written by <a href="mailto:j.jones1232@gmail.com">Jeremy Jones</a>.<br>
-    Last modified: Mon June 6<sup>th</sup> 2019.
-  </address>
+	<address>
+		Written by <a href="mailto:j.jones1232@gmail.com">Jeremy Jones</a>.<br>
+		Last modified: Mon June 6<sup>th</sup> 2019.
+	</address>
 </footer>
 </html>

34b9ecd1ab3e475d53e0622f0b07b753bba13a2c
jerome1232 <j.jones1232@gmail.com>
2019-06-13 02:46:37 -0700

    restructuring php code, adding more css styling.

--- webpage/startpi.php
+++ webpage/startpi.php
@@ -15,32 +15,25 @@
 			<a href="#contact">Contact</a>
 			<a href="#about">About</a>
 	</div>
-
-
+	<div class="pi-stream">
+		<?php
+			$ip = $_SERVER['SERVER_ADDR'];
+			echo "<img src=\"http://$ip:8080/?action=stream\" />";
+		?>
+	</div>
+	<div class="statusBar">
+		System uptime: <?php echo shell_exec('uptime -p'); ?>
+		Linux Kernel: <?php echo php_uname(); ?>
+		Ip Address: <?php echo "server ip: $ip"; ?>
+	</div>
 	<?php
-		$ip = $_SERVER['SERVER_ADDR'];
-		echo "<br><br>";
-		echo "<img src=\"http://$ip:8080/?action=stream\" />";
-		echo "<p>";
-		echo shell_exec('uptime -p');
-		echo "<br>";
-		echo php_uname();
-		echo "<br>";
 		$pidf_path = "tmp/pid";
 		$pidf = fopen($pidf_path, "r");
 		$pid = fread($pidf, filesize($pidf_path));
 		fclose($pidf);
-		echo "Previous PID: $pid, Killing it to ensure it is dead<br>";
 		shell_exec("kill $pid");
 		$cmd = "nohup ./robot_car.py >/dev/null 2>&1 &";
-		echo "Starting python script<br>";
 		shell_exec($cmd);
-		echo "Script Started, ready to roll<br>";
-		echo "If you kill the python script, just reload the page "
-			. "it will be run automatically again";
-		echo "<br>";
-		echo "server ip: $ip";
-		echo "</p>";
 	?>
 <script src='jquery/jquery-3.3.1.js'></script>
 <script src='keys.js'></script></body>

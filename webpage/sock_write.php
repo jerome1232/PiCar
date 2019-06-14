a95193bcaff15549ddedc25cf7e7fe53397a61c6
jerome1232 <j.jones1232@gmail.com>
2019-06-14 04:31:53 -0700

    add .gitignore, also added some debugging to

--- webpage/sock_write.php
+++ webpage/sock_write.php
@@ -1,12 +1,18 @@
 <?php
-$data = $_POST['key'];
-$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
-$path = $_SERVER['DOCUMENT_ROOT'].'/tmp/pySock';
-if (socket_connect($sock, $path) === false) {
-	echo "Connection failed";
-} else {
-	socket_write($sock, $data);
-	socket_shutdown($sock);
-	socket_close($sock);
-}
+	$data = $_POST['key'];
+	if (($sock = socket_create(AF_UNIX, SOCK_STREAM, 0)) == false);
+	{
+		echo "socket_create() failed: "
+		. socket_strerror(socket_last_error()) . "/n";
+		exit();
+	}
+	$path = $_SERVER['DOCUMENT_ROOT'].'/tmp/pySock';
+	if (socket_connect($sock, $path) === false) {
+		echo "Connection failed: "
+		. socket_strerror(socket_last_error()) . "/n";
+	} else {
+		socket_write($sock, $data);
+		socket_shutdown($sock);
+		socket_close($sock);
+	}
 ?>

b51f438ca8895379813840d2ae82bec365a7dfe6
jerome1232 <j.jones1232@gmail.com>
2019-06-16 21:03:36 -0700

    i for.got.a.semicolon

--- webpage/sock_write.php
+++ webpage/sock_write.php
@@ -1,6 +1,6 @@
 <?php
 	$data = $_POST['key'];
-	if (($sock = socket_create(AF_UNIX, SOCK_STREAM, 0)) == false);
+	if (($sock = socket_create(AF_UNIX, SOCK_STREAM, 0)) == false)
 	{
 		echo "socket_create() failed: "
 		. socket_strerror(socket_last_error()) . "/n";
